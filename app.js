84f391d854888eb799045b8540c608e6bb5d89cd
José Cleyton <cleytoncomp@gmail.com>
2020-07-04 19:39:06 -0300

    Deleting src folder and changing Procfile

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization , i9-token');
-    res.header('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     app.use(cors());
     next();
 });
